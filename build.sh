c4c6ff335a1342dccaa8af3f0af2f885f921d6b4
Octavio Lara <octavioasapchi@gmail.com>
2024-02-01 22:02:29 -0400

    FIX render.yaml for deploy

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 # Shell
 set -o errexit # si encuentra un error termina el proceso
 
+# Instalamos las dependencias
 pipenv install
 
+# Va a ejecutar las migraciones de la base de datos
 pipenv run upgrade
