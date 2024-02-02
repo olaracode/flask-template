c648ddf0bb13bc6ce4eceecf511007960d51e3db
Octavio Lara <octavioasapchi@gmail.com>
2024-02-01 22:25:40 -0400

    UPDATE

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,5 +1,5 @@
 # Va a permitir levantar el servior de flask en el servidor web
-from .app import app as application
+from src.app import app as application
 
 if __name__ == '__main__':
     application.run()
